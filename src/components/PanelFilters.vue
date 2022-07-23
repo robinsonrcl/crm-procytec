006e7b4ed3c74f3aea14f17c67eda5d6db02a044
Robinson Cartagena <robinsonrcl@gmail.com>
2022-07-23 13:41:34 -0500

    Se termino de construir el pnal de filtros y hacerlo responsive, tambien se hicieron algunos ajutes en el panel de historia / patologia

--- src/components/PanelFilters.vue
+++ src/components/PanelFilters.vue
@@ -12,13 +12,37 @@
     <div class="c-filtros-item">
       Estados
     </div>
-    <div class="c-filtros-item">
-      dropdown
+    <div class="c-filtros-item" id="contratos">
+      <select name="languages" id="select-contratos" multiple size="3" readonly>
+        <option value="javascript">C-1022-2012</option>
+        <option value="javascript">C-9857-2022</option>
+      </select>
     </div>
-    <div class="c-filtros-item">6</div>
-    <div class="c-filtros-item">7</div>
-    <div class="c-filtros-item">8</div>
-    <div class="c-filtros-item">9</div>
+    <div class="c-filtros-item">
+      <select name="languages" id="select-contratos" multiple size="3" readonly>
+        <option value="javascript">Rio Medellín</option>
+        <option value="javascript">La Clara</option>
+        <option value="javascript">La Tolda</option>
+      </select>
+    </div>
+    <div class="c-filtros-item" id="filtros-componentes">
+      <div><label><input type="checkbox" id="cbox1" value="first_checkbox"> AZUD</label></div>
+      <div><label><input type="checkbox" id="cbox2" value="second_checkbox"> PLACA</label></div>
+      <div><label><input type="checkbox" id="cbox2" value="second_checkbox"> MURO</label></div>
+      <div><label><input type="checkbox" id="cbox2" value="second_checkbox"> BARRAS</label></div>
+    </div>
+    <div class="c-filtros-item" id="filtros-estados">
+      <div><label><input type="checkbox" id="cbox1" value="first_checkbox"> Repotenciado</label></div>
+      <div><label><input type="checkbox" id="cbox2" value="second_checkbox"> Bueno</label></div>
+      <div><label><input type="checkbox" id="cbox2" value="second_checkbox"> En mal estado</label></div>
+      <div><label><input type="checkbox" id="cbox2" value="second_checkbox"> Otro</label></div>
+    </div>
+    <div class="c-filtros-item">
+      <div>PANEL DE FILTROS</div>
+    </div>
+    <!-- <div class="c-filtros-item">2</div>
+    <div class="c-filtros-item">3</div>
+    <div class="c-filtros-item">4</div> -->
   </div>
 </template>
 
