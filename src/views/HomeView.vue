18a9201bd5639e4c24e0cbcee6af746a39339bd2
Robinson Cartagena <robinsonrcl@gmail.com>
2022-08-10 08:19:40 -0500

    Se creo el homepage tomando como base la imagen y texto del actual sitio Bitacora - Fluvial, se adiciono un footer fijo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,39 @@
 <template>
 <div class="padre">
-  <div class="hijo">Pagina de Bienvenida a Bitacora Fluvial</div>
+  <div><img src="../assets/images/RioMedellin.png" alt=""></div>
+  <div class="hijo">
+    Los paisajes fluviales, considerados bienes públicos, 
+    requieren de monitoreo y mantenimiento, para garantizar su conservación y minimizar 
+    el riesgo de inundaciones, movimientos en masa y en general, problemas de estabilidad. 
+    Estas actividades son responsabilidad de las entidades públicas y de la población que 
+    habita en ellos.<br/><br/>
+    
+    Bitácora Fluvial es una herramienta que facilita la toma de decisiones en la ejecución 
+    de proyectos de mantenimiento, monitoreo y conservación de los recursos naturales. 
+    Esta aplicación registra, en una base de datos, la trazabilidad de la información 
+    del seguimiento realizado a corrientes de agua, evidenciando el estado de las riberas, 
+    infraestructura, asentamientos humanos y todos aquellos fenómenos naturales y 
+    antrópicos que se presentan en las cuencas de drenaje y adyacentes.<br/><br/>
+    
+    <span>
+    Los contenidos y argumentos técnicos registrados en la Bitácora fluvial son 
+    responsabilidad exclusiva de los autores de los documentos registrados para 
+    todos los efectos, y no comprometen necesariamente el punto de vista del propietario 
+    de la Base de Datos. Cualquier restricción legal que afecte los trabajos y 
+    su contenido (escrito y/o gráfico) es responsabilidad exclusiva de quienes los firman.
+    </span>
+  </div>
+  <div></div>
+  <div></div>
 </div>
+<div class="pieppal">Universidad Nacional de Colombia</div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "../components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
   components: {
-    // HelloWorld,
   },
 };
 </script>
