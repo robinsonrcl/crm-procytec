674cdfed1668d162c13c5f78bd1492d4887b784d
Robinson Cartagena <robinsonrcl@gmail.com>
2022-08-20 11:43:13 -0500

    Cambios menores

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,7 +1,7 @@
 <template>
   <Carousel ref="carousel" :wrapAround="true" :items-to-scroll="1" :autoplay="3000">
 
-    <Slide v-for="slide in newimages" :key="slide._id">
+    <Slide v-for="slide in images" :key="slide._id">
       <div class="carousel__item">
         <img class="img__modal-carousel" :src="require(`../assets/photos/${slide.src}`)" alt="" />
       </div>
