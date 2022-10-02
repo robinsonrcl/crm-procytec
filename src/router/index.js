8dc4cf4367723d01d7ec59e67c826a34c96c7a2a
Robinson Cartagena <robinsonrcl@gmail.com>
2022-10-02 15:46:32 -0500

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,10 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: HomeView,
-  },
-  {
-    path: "/fluvial",
-    name: "MapaFluvial",
-    component: () => import("../views/MapaFluvialView.vue"
-      ),
-  },
-  {
-    path: "/gestioncontratos",
-    name: "GestionContratos",
-    component: () => import("../views/GestionContratos.vue"),
-  },
-  {
-    path: "/setup",
-    name: "SetupSpa",
-    component: () => import("../views/SetupSpa.vue"),
-  },
-  {
-    path: "/profile",
-    name: "UserProfile",
-    component: () => import("../views/UserProfile.vue"),
+    component: () => import("../views/MapaFluvialView.vue"),
   }
 ];
 
